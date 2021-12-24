8a62ac60e64d1d12a16a508a8e27174d3d2a9a65
qinxiaodong <qinxiaodong@example.com>
2021-12-24 12:22:00 +0000

    add

--- git.sh
+++ git.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-cd /project/company/JesseXiaoDong.github.io & 
-git add . &
-git commit -m "add" &
+cd /project/company/JesseXiaoDong.github.io 
+ls 
+git add . 
+sleep 1s
+git commit -m "add" 
+sleep 1s
 git push 
